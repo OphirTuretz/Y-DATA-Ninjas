2885f2afba557369a7a2f4cdc65dc16b62917f62
maymena <may.menachem@gmail.com>
2025-02-11 11:16:37 +0200

    new features branch

--- preprocess.py
+++ preprocess.py
@@ -243,8 +243,9 @@ def preprocess(df: pd.DataFrame) -> pd.DataFrame:
         "current_same_campaign_freq",  #
         "current_same_product_freq",  #
 
-        'click_ratio2',
-        # 'click_ratio_prod',
+        # "product_category_2"
+        # 'click_ratio2',
+        'click_ratio_prod',
         'user_max_click_ratio2',
         'click_ratio1',
         'user_max_click_ratio1',
